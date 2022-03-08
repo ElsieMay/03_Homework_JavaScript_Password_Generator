5540434845989b615f2678717a40a14c4573b254
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-08 14:50:41 +1100

    added variables for confirmations, listed variables

--- develop/script.js
+++ develop/script.js
@@ -1,15 +1,44 @@
-// Assignment Code
+// Assignment Codes
 var generateBtn = document.querySelector("#generate");
+var SpcCharcfm;
+var Numbrscfm;
+var Lletterscfm;
+var Cletterscfm;
+var Cletters;
+var Lletters;
+var Numbers;
+var SpcChar;
+var pwd;
+var passLen;
+var password;
+var passwordText;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-String letters= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-String Numbers= "0123456789";
-String splcChar+ "!@#$%^&*";
-  passwordText.value = password;
+	var Password = generatePassword();
 
+	var passwordText = document.querySelector("#password");
+}
+// passwordText.value = password;
+
+function generatePassword() {
+	const Cletters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+	const Lletters = ["abcdefghijklmnopqrstuvwxyz"];
+	const Numbers = ["0123456789"];
+	const SpcChar = ["!@#$%^&*"];
+	var pwd = [];
+
+	do {
+		var passLen = prompt("Please enter the password length required");
+	} while (passLen < 8 || passLen > 128);
+
+	var Cletterscfm = confirm("Would you like to include capital letters?");
+
+	var Lletterscfm = confirm("Would you like to include lowercase letters?");
+
+	var Numbrscfm = confirm("Would you like to include numebrs?");
+
+	var SpcCharcfm = confirm("Would you like to include special characters?");
 }
 
 // Add event listener to generate button
