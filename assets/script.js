d742fe1c4551025554ad84fab07e442b4b81da8a
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-09 22:16:53 +1100

    added all commenting

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 // Assignment Codes
+//These are a comprehensive list of variables used in below code
 var generateBtn = document.querySelector("#generate");
 var SpcCharcfm;
 var Numbrscfm;
@@ -19,13 +20,13 @@ function writePassword() {
 	var passwordText1 = document.querySelector("#password");
 	passwordText1.value = password;
 }
-
+//This function performs selected criteria from user and provides a randomised output
 function generatePassword() {
 	const Cletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 	const Lletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	const Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 	const SpcChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
+	//These do while loops specify password length parameters and prompt responses
 	do {
 		passLen = prompt("Please enter the password length required");
 	} while (passLen < 8 || passLen > 128);
@@ -36,7 +37,7 @@ function generatePassword() {
 		Numbrscfm = confirm("Would you like to include numbers?");
 		SpcCharcfm = confirm("Would you like to include special characters?");
 	} while (Cletterscfm === false && Lletterscfm === false && Numbrscfm === false && SpcCharcfm === false);
-
+	//I have concatenated the above variables below
 	if (Cletterscfm) {
 		pwd = pwd.concat(Cletters);
 	}
@@ -49,11 +50,13 @@ function generatePassword() {
 	if (SpcCharcfm) {
 		pwd = pwd.concat(SpcChar);
 	}
+	//The below creates a randomised password for the user, using the above data
 	for (var i = 0; i < passLen; i++) {
 		passwordText = passwordText + pwd[Math.floor(Math.random() * pwd.length)];
 	}
+	//Here we are calling the passwordText variable
 	return passwordText;
 }
 
-// Add event listener to generate button
+// Event listener for generate button
 generateBtn.addEventListener("click", writePassword);
